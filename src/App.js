346921b46dc1f4c626f40ac0a656c2f35af318e5
AnabellaSimonpietri <simonpietrianabella@gmail.com>
2023-03-16 22:11:14 -0300

    app dinámica con estados

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import Card from "./components/Card/Card.jsx";
 import Cards from "./components/Cards/Cards.jsx";
-import SearchBar from "./components/SearchBar/SearchBar.jsx";
-import characters from "./data.js";
+import Nav from "./components/Nav/Nav.jsx";
+import { useState } from "react";
 import style from "./App.modules.css";
 
 function App() {
+  const onSearch = (id) => {
+    const URL_BASE = "https://be-a-rym.up.railway.app/api";
+    const KEY = "dda0e615d14c.cfb009cdce3a34cdfd7f";
+
+    fetch(`${URL_BASE}/character/${id}?key=${KEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.name && !characters.find((char) => char.id === data.id)) {
+          setCharacters((oldChars) => [...oldChars, data]);
+        } else {
+          alert("Error");
+        }
+      });
+  };
+
+  const onClose = (id) => {
+    setCharacters(characters.filter((char) => char.id !== id));
+  };
+
+  const [characters, setCharacters] = useState([]);
   return (
     <div className="App" style={{ padding: "25px" }}>
-      <div className="nav">
-        <SearchBar onSearch={(characterID) => window.alert(characterID)} />
+      <div className={style.nav}>
+        <Nav onSearch={onSearch} />
       </div>
       <div>
-        <Cards characters={characters} />
+        <Cards characters={characters} onClose={onClose} />
       </div>
-      <hr />
     </div>
   );
 }
--- src/components/Cards/styledComponents.js
+++ src/components/Cards/styledComponents.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const CardsContainer = styled.div`
- display: flex;
- flex-direction; row;
- justify-content; space-between;
- width: center;
- margin: auto; 
- border-radious: 15px;
- 
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 80%;
+  margin: auto;
+  padding: 2em;
+  border-radius: 15px;
 `;
